a711951a71267b7f001ac46b257f8bcdfcd4c6ec
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-03-17 16:16:25 +0700

    Add: edit & delete endpoint

--- src/utils/APIs.js
+++ src/utils/APIs.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
-
 const url = 'https://memories-saya.herokuapp.com';
 const config = token => ({
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
-export const fetchPosts = () => axios.get(url + `/posts`);
-export const fetchPostsPerPage = page => axios.get(url + `/posts?page=${page}`);
+
+export const fetchPosts = () => axios.get(`${url}/posts`);
+export const fetchPostsPerPage = page => axios.get(`${url}/posts?page=${page}`);
 export const fetchComment = (value, id, token) =>
   axios.post(`${url}/posts/${id}/commentPost`, value, config(token));
 export const fetchPostsBySearch = searchQuery =>
-  axios.get(url + `/posts/searchQuery=${searchQuery}`);
+  axios.get(`${url}/posts/searchQuery=${searchQuery}`);
 export const likeAPost = (id, token) =>
   axios.patch(`${url}/posts/${id}/likePost`, null, config(token));
-export const createPost = formData =>
-  axios.post(url + '/posts', formData, config);
+export const createPostAPI = (formData, token) =>
+  axios.post(`${url}/posts`, formData, config(token));
+export const editPostAPI = (formData, token, id) =>
+  axios.patch(`${url}/posts/${id}`, formData, config(token));
+export const deletePostAPI = (id, token) =>
+  axios.delete(`${url}/posts/${id}`, config(token));
 
-export const signIn = formData => axios.post(url + '/user/signin', formData);
-export const signUp = formData => axios.post(url + '/user/signup', formData);
+export const signIn = formData => axios.post(`${url}/user/signin`, formData);
+export const signUp = formData => axios.post(`${url}/user/signup`, formData);
