9e8e77bf0c4bc764bda6fdcff26f6ba8a4fc40ab
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-03-18 16:29:00 +0700

    update: theme. Add: right icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,57 @@ import {StyleSheet, Text, View, TouchableNativeFeedback} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Gap from './Gap';
+import colors from '../utils/colors';
 
-export default function Header({leftIcon, title = 'Header', onPressLeft}) {
+export default function Header({
+  leftIcon,
+  title = 'Header',
+  onPressLeft,
+  rightIcon,
+  onPressRight,
+}) {
   const widthGap = leftIcon ? 15 : 25;
+  const {background, text} = colors();
   return (
-    <View style={styles.container}>
+    <View style={styles.container(background)}>
       {leftIcon && (
         <TouchableNativeFeedback
           useForeground
           onPress={onPressLeft}
           background={TouchableNativeFeedback.Ripple(null, null, 20)}>
           <View style={styles.viewIcon}>
-            <Icon name={leftIcon} size={27} color="white" style={styles.icon} />
+            <Icon name={leftIcon} size={27} color={text} style={styles.icon} />
           </View>
         </TouchableNativeFeedback>
       )}
       <Gap width={widthGap} />
-      <Text style={styles.textTitle}>{title}</Text>
+      <Text style={styles.textTitle(text)}>{title}</Text>
+      {rightIcon && (
+        <TouchableNativeFeedback
+          useForeground
+          onPress={onPressRight}
+          background={TouchableNativeFeedback.Ripple(null, null, 20)}>
+          <View style={{...styles.viewIcon, marginRight: 5}}>
+            <Icon
+              name={rightIcon}
+              size={27}
+              color={'tomato'}
+              style={styles.icon}
+            />
+          </View>
+        </TouchableNativeFeedback>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  textTitle: {
-    color: 'white',
+  textTitle: color => ({
+    color,
     fontWeight: 'bold',
     fontSize: 17,
-  },
+    flex: 1,
+  }),
   icon: {
     flex: 1,
     textAlign: 'center',
@@ -40,13 +64,13 @@ const styles = StyleSheet.create({
     borderRadius: 50 / 2,
     overflow: 'hidden',
   },
-  container: {
+  container: bg => ({
     width: '100%',
     maxWidth: 720,
     alignSelf: 'center',
-    backgroundColor: 'transparent',
+    backgroundColor: bg,
     flexDirection: 'row',
     alignItems: 'center',
     height: 50,
-  },
+  }),
 });
