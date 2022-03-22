33d55410622192edf06218a1e0c141e1197ab64d
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-03-22 15:44:35 +0700

    cleaning code

--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -1,25 +1,14 @@
-import {View, Button} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import {Posts} from '../../components/exports';
 import {CStatusBar} from '../../components/exports';
 import colors from '../../utils/colors';
-import {THEME} from '../../redux/actionTypes';
 import {useDispatch} from 'react-redux';
 export default function Dashboard({navigation}) {
-  const dispatch = useDispatch();
-  const {background, selectedTheme} = colors();
+  const {background} = colors();
   return (
     <View style={{flex: 1, backgroundColor: background}}>
       <CStatusBar />
-      <Button
-        title={`current theme: ${selectedTheme}. tap to change`}
-        onPress={() =>
-          dispatch({
-            type: THEME,
-            payload: selectedTheme == 'dark' ? 'light' : 'dark',
-          })
-        }
-      />
       <Posts navigation={navigation} />
     </View>
   );
