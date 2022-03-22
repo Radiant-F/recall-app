ef7560469db364e5e4cbcfefc393cac160d55349
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-03-22 15:44:23 +0700

    update: styling

--- src/screens/auth/Auth.js
+++ src/screens/auth/Auth.js
@@ -1,12 +1,14 @@
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, Button} from 'react-native';
 import React from 'react';
 import {SignIn, CStatusBar, Background, SignUp} from '../../components/exports';
 import {useSelector} from 'react-redux';
+import colors from '../../utils/colors';
 
 export default function Auth({navigation}) {
   const {auth} = useSelector(state => state);
+  const {background} = colors();
   return (
-    <View style={{backgroundColor: '#2e3134', flex: 1}}>
+    <View style={{backgroundColor: background, flex: 1}}>
       <CStatusBar />
       <View style={styles.container}>
         {auth.is_login && <SignIn navigation={navigation} />}
--- src/screens/auth/Splash.js
+++ src/screens/auth/Splash.js
@@ -1,14 +1,13 @@
 import {StyleSheet, Text, View, Button, ActivityIndicator} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import EncryptedStorage from 'react-native-encrypted-storage';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {postSignIn} from '../../redux/actions/auth';
 import {CStatusBar} from '../../components/exports';
-import {THEME} from '../../redux/actionTypes';
 import colors from '../../utils/colors';
 
 export default function Splash({navigation}) {
-  const {background, postCard, selectedTheme, subtext, text} = colors();
+  const {background, text} = colors();
   const dispatch = useDispatch();
   useEffect(() => {
     updateSession();
@@ -18,17 +17,13 @@ export default function Splash({navigation}) {
     if (session) {
       const formData = JSON.parse(session);
       dispatch(postSignIn(formData, navigation));
-    } else return setTimeout(() => navigation.replace('Auth'), 2000);
+    } else return setTimeout(() => navigation.replace('Auth'), 1000);
   }
 
   return (
     <View style={styles.container(background)}>
       <CStatusBar />
-
-      <ActivityIndicator
-        size="large"
-        color={selectedTheme == 'dark' ? 'white' : 'black'}
-      />
+      <ActivityIndicator size="large" color={text} />
     </View>
   );
 }
