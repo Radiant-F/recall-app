9cd7f84a5ef91b960038b2f05329030522bf6ee7
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-03-22 15:41:16 +0700

    update: styling

--- src/components/buttons/FloatingButton.js
+++ src/components/buttons/FloatingButton.js
@@ -2,10 +2,14 @@ import {StyleSheet, Text, View, TouchableNativeFeedback} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function FloatingButton({onPress, icon = 'book-plus-multiple'}) {
+export default function FloatingButton({
+  onPress,
+  icon = 'book-plus-multiple',
+  absolute = true,
+}) {
   return (
     <TouchableNativeFeedback useForeground onPress={onPress}>
-      <View style={styles.view}>
+      <View style={absolute ? styles.view : styles.regular}>
         <Icon name={icon} style={styles.icon} size={27} />
       </View>
     </TouchableNativeFeedback>
@@ -17,6 +21,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     textAlignVertical: 'center',
     flex: 1,
+  },
+  regular: {
+    width: 60,
+    height: 60,
+    borderRadius: 60 / 2,
+    elevation: 3,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    alignSelf: 'flex-end',
+    marginBottom: 20,
+    marginRight: 20,
   },
   view: {
     width: 60,
