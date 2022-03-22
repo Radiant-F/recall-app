a36b7d95cd1f102a9c2c903fb3877fdb44c42ff7
RadiantF-pondok <radiantfadilah0@gmail.com>
2022-03-22 15:42:51 +0700

    Add: settings screen

--- src/screens/exports.js
+++ src/screens/exports.js
@@ -4,5 +4,6 @@ import Splash from './auth/Splash';
 import Dashboard from './dashboard/Dashboard';
 import PostForm from './posts/PostForm';
 import Profile from './profile/Profile';
+import Settings from './settings/Settings';
 
-export {Dashboard, Auth, Splash, Demo, PostForm, Profile};
+export {Dashboard, Auth, Splash, Demo, PostForm, Profile, Settings};
